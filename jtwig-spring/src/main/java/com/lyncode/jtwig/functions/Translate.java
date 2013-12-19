94bf6c8b1c9eb47ec624f8e192747e4befb51ffa
João Melo <jmelo@lyncode.com>
2013-12-19 20:47:48 +0000

    Added maven source plugin version and implemented Translate function.

--- jtwig-spring/src/main/java/com/lyncode/jtwig/functions/Translate.java
+++ jtwig-spring/src/main/java/com/lyncode/jtwig/functions/Translate.java
@@ -17,10 +17,38 @@
 package com.lyncode.jtwig.functions;
 
 import com.lyncode.jtwig.functions.exceptions.FunctionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
+import static com.lyncode.jtwig.util.LocalThreadHolder.getApplicationContext;
+import static com.lyncode.jtwig.util.LocalThreadHolder.getServletRequest;
+import static java.util.Arrays.copyOfRange;
 
 public class Translate implements Function {
     @Override
     public Object execute(Object... arguments) throws FunctionException {
-        return null;
+        ApplicationContext applicationContext = getApplicationContext();
+        MessageSource messageSource = getMessageSource(applicationContext);
+
+        if (arguments.length < 1) throw new FunctionException("Expecting at least one argument");
+        else {
+            HttpServletRequest request = getServletRequest();
+            Locale locale = getLocaleResolver(applicationContext).resolveLocale(request);
+            Object[] parameters = copyOfRange(arguments, 1, arguments.length);
+
+            return messageSource.getMessage(String.valueOf(arguments[0]), parameters, locale);
+        }
+    }
+
+    private LocaleResolver getLocaleResolver (ApplicationContext applicationContext) {
+        return applicationContext.getBean(LocaleResolver.class);
+    }
+
+    private MessageSource getMessageSource(ApplicationContext applicationContext) {
+        return applicationContext.getBean(MessageSource.class);
     }
 }
