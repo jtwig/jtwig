390fb4371eadbbe4469dfdf7717dd610f0c72b48
João Melo <jmelo@lyncode.com>
2014-03-06 18:43:00 +0000

    Render function added
    Stub Request and Responses moved to it's own classes.
    
    Fixed jtwig after rebase
    
    Changed the spring tests, to use a real server.
    
    Stub Request and Responses moved to it's own classes.
    
    Fixed jtwig after rebase
    
    Changed the spring tests, to use a real server.
    
    Fixed spring functions

---
+++ jtwig-spring-test/src/test/java/com/lyncode/acceptance/functions/RenderTest.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lyncode.acceptance.functions;
+
+import com.lyncode.acceptance.AbstractJtwigAcceptanceTest;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.lyncode.jtwig.GetMethodMatchers.body;
+import static com.lyncode.jtwig.SyntacticSugar.then;
+import static com.lyncode.jtwig.SyntacticSugar.when;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@Controller
+public class RenderTest extends AbstractJtwigAcceptanceTest {
+    @RequestMapping("/")
+    public String action () {
+        return "render/test";
+    }
+
+    @RequestMapping("/test")
+    public ResponseEntity<String> test () {
+        return new ResponseEntity<>("k", HttpStatus.OK);
+    }
+
+    @Test
+    public void renderTest() throws Exception {
+        when(serverReceivesGetRequest("/"));
+        then(theGetResult(), body(is(equalTo("ok"))));
+    }
+}
