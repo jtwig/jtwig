cd351f2753d9aab8a18acd0e3e5c63ca8f40dcae
Thomas Wilson <thomas.wilson@crosstreelabs.com>
2015-02-11 09:06:09 -0500

    Added some more tests for PR #263

--- jtwig-core/src/test/java/org/jtwig/unit/expressions/model/ValueListTest.java
+++ jtwig-core/src/test/java/org/jtwig/unit/expressions/model/ValueListTest.java
@@ -26,6 +26,8 @@ import static org.jtwig.expressions.model.ValueList.create;
 import org.jtwig.parser.model.JtwigPosition;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class ValueListTest extends AbstractJtwigTest {
     private static final JtwigPosition POSITION = new JtwigPosition(null, 1, 1);
 
@@ -34,6 +36,33 @@ public class ValueListTest extends AbstractJtwigTest {
         ValueList list = create(null, new Constant(1), new Constant(2));
         Object result = list.compile(compileContext).calculate(renderContext);
         assertThat(result, is(instanceOf(List.class)));
+    }
+    
+    @Test
+    public void ensureCharacterLimitsWork() throws Exception {
+        ValueList list = create(null, new Constant('a'), new Constant('c'));
+        List result = (List)list.compile(compileContext).calculate(renderContext);
+        assertEquals('a', result.get(0));
+        assertEquals('b', result.get(1));
+        assertEquals('c', result.get(2));
+    }
+    
+    @Test
+    public void ensureInvertedNumericLimitsWork() throws Exception {
+        ValueList list = create(null, new Constant(5), new Constant(1));
+        List result = (List)list.compile(compileContext).calculate(renderContext);
+        assertEquals(5, result.get(0));
+        assertEquals(3, result.get(2));
+        assertEquals(1, result.get(4));
+    }
+    
+    @Test
+    public void ensureInvertedCharacterLimitsWork() throws Exception {
+        ValueList list = create(null, new Constant('e'), new Constant('a'));
+        List result = (List)list.compile(compileContext).calculate(renderContext);
+        assertEquals('e', result.get(0));
+        assertEquals('c', result.get(2));
+        assertEquals('a', result.get(4));
     }
 
     @Test(expected = ParseBypassException.class)
@@ -47,5 +76,9 @@ public class ValueListTest extends AbstractJtwigTest {
     @Test(expected = ParseBypassException.class)
     public void invalidLimitTypes3() throws Exception {
         create(POSITION, new Constant("a"), new Constant("b"));
+    }
+    @Test(expected = ParseBypassException.class)
+    public void invalidLimitTypes4() throws Exception {
+        create(POSITION, new Constant(new Object()), new Constant(new Object()));
     }
 }
