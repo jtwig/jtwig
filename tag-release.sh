9584d7cce9593f36fb2d177657845d5e5c4db8ba
Joao Melo <jmelo@lyncode.com>
2015-02-14 11:29:36 +0000

    Adding tag releasing after merge

---
+++ tag-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION=`head -n 1 tag.version`
+
+if [ "$VERSION" != "" ]; then
+    API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
+    curl --data "$API_JSON" https://api.github.com/repos/jtwig/jtwig/releases?access_token=$GITHUB_API_KEY
+fi
